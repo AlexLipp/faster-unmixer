254c0f4f5f4c5722c65eb3519755a222e93e41e5
Alex Lipp <alex@lipp.org.uk>
2023-06-21 08:40:51 +0100

    Convert synthetic_test into pass/fail unit test

--- synthetic_test.py
+++ synthetic_test.py
@@ -12,13 +12,15 @@ sample_network, _ = snu.get_sample_graphs(
     sample_data_filename="data/synthetic_samples.dat",
 )  # Get upstream basins
 
-plt.figure(figsize=(15, 10))  # Visualise network
-plt.title("Sample Network")
-snu.plot_network(sample_network)
+# plt.figure(figsize=(15, 10))  # Visualise network
+# plt.title("Sample Network")
+# snu.plot_network(sample_network)
 
 areas = snu.get_unique_upstream_areas(sample_network)
 
 # Pick random values to set each basin
+np.random.seed(0)
+print("Generating synthetic data...")
 synth_upst_concs = {sample: max_val * np.random.rand() for sample in areas.keys()}
 
 # Pick random export rates to set for each sub-basin
@@ -27,6 +29,7 @@ input_export_rates = {sample: np.random.rand() for sample in areas.keys()}
 # Generate synthetic upstream concentration map
 upst_conc_map = snu.get_upstream_concentration_map(areas, synth_upst_concs)
 # Predict concentration at downstream observation points
+print("Predicting downstream concentrations...")
 mixed_synth_down, _ = snu.mix_downstream(
     sample_network, areas, upst_conc_map, export_rates=input_export_rates
 )
@@ -36,12 +39,14 @@ mixed_synth_down, _ = snu.mix_downstream(
 #     for sample, value in mixed_synth_down.items()
 # }
 # Set up problem
+print("Building problem...")
 problem = snu.SampleNetworkUnmixer(sample_network, use_regularization=False)
 # Solve problem using synthetic downstream observations as input
 recovered_down, recovered_up = problem.solve(
     mixed_synth_down, solver="ecos", export_rates=input_export_rates
 )
 # Generate recovered upstream concentration map
+print("Generating recovered upstream concentration map...")
 recovered_conc_map = snu.get_upstream_concentration_map(areas, recovered_up)
 
 # Extract arrays of predicted and `observed' concentrations
@@ -50,7 +55,30 @@ down_obs = [mixed_synth_down[sample] for sample in recovered_down.keys()]
 up_preds = [recovered_up[sample] for sample in recovered_down.keys()]
 up_obs = [synth_upst_concs[sample] for sample in recovered_down.keys()]
 
+print("Testing results...")
+tolerance = 1e-5  # Tolerance for asserting equality of values
+# Assert down_preds and down_obs contain equal values within a tolerance
+if np.allclose(down_preds, down_obs, rtol=tolerance):
+    print("SUCCESS: Downstream concentrations match to within tolerance of {}%".format(tolerance))
+else:
+    raise AssertionError(
+        "WARNING: Inputted and recovered downstream concentrations do not match with tolerance of {}%".format(
+            tolerance
+        )
+    )
+# Assert up_preds and up_obs contain equal values within a tolerance
+if np.allclose(up_preds, up_obs, rtol=tolerance):
+    print("SUCCESS: Upstream concentrations match to within tolerance of {}%".format(tolerance))
+else:
+    raise AssertionError(
+        "WARNING: Inputted and recovered upstream concentrations do not match with tolerance of {}%".format(
+            tolerance
+        )
+    )
+
+print("SUCCESS: All tests passed!")
 # Visualise results of synthetic test
+print("Visualising results...")
 plt.figure(figsize=(15, 15))
 plt.subplot(2, 2, 1)
 plt.title("Inputted")
