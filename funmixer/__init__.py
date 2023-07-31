4d500d79880efdd6a8e135ff02d132039aeb9f57
Richard Barnes <rbarnes@umn.edu>
2023-07-31 12:34:38 -0700

    Export symbols

--- funmixer/__init__.py
+++ funmixer/__init__.py
@@ -1,11 +1,27 @@
 from .sample_network_unmix import (
-    SampleNetworkUnmixer,
+    ElementData,
     get_element_obs,
     get_sample_graphs,
     get_unique_upstream_areas,
     get_upstream_concentration_map,
     mix_downstream,
+    nx_get_downstream,
     plot_network,
     plot_sweep_of_regularizer_strength,
+    SampleNetworkUnmixer,
     visualise_downstream,
 )
+
+__all__ = [
+    "ElementData",
+    "get_element_obs",
+    "get_sample_graphs",
+    "get_unique_upstream_areas",
+    "get_upstream_concentration_map",
+    "mix_downstream",
+    "nx_get_downstream",
+    "plot_network",
+    "plot_sweep_of_regularizer_strength",
+    "SampleNetworkUnmixer",
+    "visualise_downstream",
+]
