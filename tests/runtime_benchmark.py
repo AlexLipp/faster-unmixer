f6a3cd71778a42554ce605b5c90b565e6509d171
Richard Barnes <rbarnes@umn.edu>
2023-08-07 20:50:06 -0700

    Improve plotting. Include solver times.

--- tests/runtime_benchmark.py
+++ tests/runtime_benchmark.py
@@ -1,3 +1,4 @@
+import sys
 import time
 from collections import defaultdict
 from dataclasses import dataclass, field
@@ -6,6 +7,7 @@ from typing import DefaultDict, List, Optional, Tuple, TypeVar
 # pyre-fixme[21]: Could not find module `matplotlib.pyplot`.
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 import tqdm
 
 # pyre-fixme[21]: Could not find module `random_networks_test`.
@@ -33,10 +35,13 @@ MAXIMUM_AREA = 1e2
 BRANCHING_FACTOR = 3
 
 # Set maximum number of nodes to try
-MAXIMUM_NETWORK_SIZE = 500
+MAXIMUM_NETWORK_SIZE = 250
 
 # Set number of networks to test
 NUMBER_OF_NETWORKS = 100
+
+# Number of times to run the test to average run-time variations
+TEST_REPEATS = 1
 
 
 @dataclass
@@ -48,59 +53,59 @@ class BenchmarkResults:
     solver_times: DefaultDict[int, List[float]] = field(default_factory=lambda: defaultdict(list))
 
 
-def bavg(x: DefaultDict[int, List[float]]) -> List[float]:
-    return [float(np.mean(np.array(x[n]))) for n in sorted(x.keys())]
+def bavg(x: DefaultDict[int, List[float]], scale: float) -> List[float]:
+    return [scale * float(np.mean(np.array(x[n]))) for n in sorted(x.keys())]
 
 
-def bstd(x: DefaultDict[int, List[float]]) -> List[float]:
-    return [float(np.std(np.array(x[n]))) for n in sorted(x.keys())]
+def bstd(x: DefaultDict[int, List[float]], scale: float) -> List[float]:
+    return [scale * float(np.std(np.array(x[n]))) for n in sorted(x.keys())]
 
 
 def plot_first_second(
-    network_sizes: List[int], b: BenchmarkResults, name: str, c1: str, c2: str
+    network_sizes: List[int],
+    b: BenchmarkResults,
+    name: str,
+    c1: str,
+    c2: str,
+    scale: float = 1,
+    symbol: str = "o-",
 ) -> None:
-    plt.plot(
+    plt.errorbar(
         network_sizes,
-        bavg(b.first_solves),
-        "o-",
+        bavg(b.first_solves, scale=scale),
+        fmt=symbol,
+        yerr=bstd(b.first_solves, scale=scale),
+        label=f"{name} Total Time (1$^{{st}}$ solve)",
         c=c1,
-        label=f"{name} (1$^{{st}}$ solve)",
         markersize=4,
     )
     plt.errorbar(
         network_sizes,
-        bavg(b.first_solves),
-        yerr=bstd(b.first_solves),
-    )
-    plt.plot(
-        network_sizes,
-        bavg(b.subsequent_solves),
-        "o-",
+        bavg(b.subsequent_solves, scale=scale),
+        yerr=bstd(b.subsequent_solves, scale=scale),
         c=c2,
-        # Make points smaller
+        label=f"{name} Total Time (2$^{{nd}}$ solve)",
+        fmt=symbol,
         markersize=4,
-        label=f"{name} (2$^{{nd}}$ solve)",
     )
+
+
+def plot_solver_time(
+    network_sizes: List[int],
+    b: BenchmarkResults,
+    name: str,
+    c: str,
+    scale: float = 1,
+    symbol: str = "o-",
+) -> None:
     plt.errorbar(
         network_sizes,
-        bavg(b.subsequent_solves),
-        yerr=bstd(b.subsequent_solves),
-    )
-
-
-def plot_solver_time(network_sizes: List[int], b: BenchmarkResults, name: str, c: str) -> None:
-    plt.plot(
-        network_sizes,
-        bavg(b.solver_times),
-        "o-",
+        bavg(b.solver_times, scale=scale),
+        yerr=bstd(b.solver_times, scale=scale),
         c=c,
-        label=f"{name}",
         markersize=4,
-    )
-    plt.errorbar(
-        network_sizes,
-        bavg(b.solver_times),
-        yerr=bstd(b.solver_times),
+        fmt=symbol,
+        label=f"{name}",
     )
 
 
@@ -109,7 +114,7 @@ def none_throws(x: Optional[T]) -> T:
     return x
 
 
-def run_benchmark(solver: str, sizes: List[int], repeats: int = 10) -> BenchmarkResults:
+def run_benchmark(solver: str, sizes: List[int], repeats: int = TEST_REPEATS) -> BenchmarkResults:
     ret = BenchmarkResults()
 
     for n in tqdm.tqdm(sizes):
@@ -135,7 +140,7 @@ def run_benchmark(solver: str, sizes: List[int], repeats: int = 10) -> Benchmark
     return ret
 
 
-def main() -> None:
+def do_benchmark() -> None:
     # Generate list of network sizes
     network_sizes: List[int] = np.unique(
         np.rint(
@@ -169,15 +174,24 @@ def main() -> None:
     print("Testing SCS solver...")
     scs_bench = run_benchmark("scs", network_sizes)
 
+    with open("benchmark_results.pkl", "wb") as fout:
+        pickle.dump([network_sizes, ecos_bench, gurobi_bench, scs_bench], fout)
+
     end = time.time()
     print(f"Benchmarking took {end - start} seconds.")
     print("Benchmark complete.")
     print("#" * 80)
+
+
+def plot_benchmark() -> None:
+    with open("benchmark_results.pkl", "rb") as fin:
+        network_sizes, ecos_bench, gurobi_bench, scs_bench = pickle.load(fin)
+
     # Plot results of total runtime
     # Plot solve time against number of nodes
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 5))
 
-    plt.subplot(2, 1, 1)
+    # plt.subplot(2, 1, 1)
     plot_first_second(network_sizes, ecos_bench, "ECOS", "#1f78b4", "#a6cee3")
     plot_first_second(network_sizes, scs_bench, "SCS", "#e31a1c", "#fb9a99")
     if gurobi_bench:
@@ -189,13 +203,15 @@ def main() -> None:
     plt.ylabel("Solve time (s)")
     plt.legend()
     plt.grid(True, which="both")
-    plt.subplot(2, 1, 2)
+    # plt.subplot(2, 1, 2)
 
     # Plot results of just the CVXPY solve time
-    plot_solver_time(network_sizes, ecos_bench, "ECOS", "#1f78b4")
-    plot_solver_time(network_sizes, scs_bench, "SCS", "#e31a1c")
+    plot_solver_time(network_sizes, ecos_bench, "ECOS Solver Time", "#1f78b4", symbol="o--")
+    plot_solver_time(
+        network_sizes, scs_bench, "SCS Solver Time", "#e31a1c", scale=1e-3, symbol="o--"
+    )
     if gurobi_bench:
-        plot_solver_time(network_sizes, gurobi_bench, "GUROBI", "#33a02c")
+        plot_solver_time(network_sizes, gurobi_bench, "GUROBI Solver Time", "#33a02c", symbol="o--")
 
     plt.xscale("log")
     plt.title("Optimizer time")
@@ -207,6 +223,17 @@ def main() -> None:
     plt.tight_layout()
     plt.savefig("runtime_benchmark.png", dpi=400, bbox_inches="tight")
     plt.show()
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        raise ValueError("Need to specify `run` or `plot`")
+    if sys.argv[1] == "run":
+        do_benchmark()
+    elif sys.argv[1] == "plot":
+        plot_benchmark()
+    else:
+        raise ValueError("Unknown command: need `run` or `plot`")
 
 
 if __name__ == "__main__":
